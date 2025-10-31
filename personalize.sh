71b0665e8a8aa64acb8c515c25e1a79f5870c905
yasser.zaheer <yasserizaheer@gmail.com>
2025-10-31 12:08:24 +1100

    fix: update repository URLs and image names to reflect new ownership

--- personalize.sh
+++ personalize.sh
@@ -1,23 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ -z "$1" ]; then
-  echo "./personalize.sh <username> [repository_name] [image_name]"
+set -euo pipefail
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: ./personalize.sh <username> [repository_name] [image_name]"
   exit 1
 fi
 
-username=$1
-repository_name=$2
-image_name=$3
+username="$1"
+repository_name="${2-}"
+image_name="${3-}"
 
-find . -type f -name '*.yaml' -exec sed -E -i.bak s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
-find . -type f -name '*.yaml' -exec sed -E -i.bak s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
+# Helper: run a sed replace in-place with a .bak backup, then remove the backup
+replace_in_file() {
+  local file="$1"
+  local pattern="$2"
+  local replacement="$3"
+  sed -E -i.bak "s#${pattern}#${replacement}#g" "$file"
+  rm -f "${file}.bak"
+}
 
-if [ ! -z "$repository_name" ]; then
-  find . -type f -name '*.yaml' -exec sed -E -i.bak "s#https://github.com/${username}/[-_a-zA-Z0-9]+#https://github.com/${username}/${repository_name}#g" {} +
+export username repository_name image_name
+
+while IFS= read -r -d '' f; do
+  replace_in_file "$f" 'https://github.com/[-_a-zA-Z0-9]+' "https://github.com/${username}"
+  replace_in_file "$f" 'ghcr.io/[-_a-zA-Z0-9]+' "ghcr.io/${username}"
+done < <(find . -type f -name '*.yaml' -print0)
+
+if [ -n "$repository_name" ]; then
+  while IFS= read -r -d '' f; do
+    replace_in_file "$f" "https://github.com/${username}/[-_a-zA-Z0-9]+" "https://github.com/${username}/${repository_name}"
+  done < <(find . -type f -name '*.yaml' -print0)
 fi
 
-if [ ! -z "$image_name" ]; then
-  find . -type f -name '*.yaml' -exec sed -E -i.bak "s#ghcr.io/${username}/[-_a-zA-Z0-9]+#ghcr.io/${username}/${image_name}#g" {} +
+if [ -n "$image_name" ]; then
+  while IFS= read -r -d '' f; do
+    replace_in_file "$f" "ghcr.io/${username}/[-_a-zA-Z0-9]+" "ghcr.io/${username}/${image_name}"
+  done < <(find . -type f -name '*.yaml' -print0)
 fi
 
-find . -type f -name '*.bak' -exec rm -f {} +
+exit 0
